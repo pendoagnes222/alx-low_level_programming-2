356660e77e3aaaf4613741e823bac8d68f711767
nthenyakyalo <nthenyakyalo202@gmail.com>
2022-10-11 13:22:28 -0700

    poppy

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,1 @@
-0x0E. C - Structures, typedef
+0.defining a newtype struct dog
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,31 @@
-#include "dog.h"
-#include <stdio.h>
+#ifndef DOG
+#define DOG
 /**
- * init_dog -initialize new dog prop
- * @d: new dog
- * @name: new dog's name
- * @age: new dog age
- * @owner: new dog owner
+ * struct dog -new dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
  *
- * Description: initialize new dog object
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+ * Description: dog object having 3 props
+ *  */
+struct dog
 {
-	if (d == NULL)
-		return;
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
-}
+       char *name;
+       float age;
+       char *owner;
+};
+/**
+ * typedef struct -define new object
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: define new data type
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void str_cpy(char *src, char *dest);
+#end
